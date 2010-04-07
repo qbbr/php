f1a698bd91c8c64c88ddf065553a2ace75f0fa2a
Sokolov Innokenty <r2.kenny@gmail.com>
2010-04-07 13:50:27 +0400

    Q.magicImage #fix copyright pic

--- Q.magicImage/Q.magicImage.php
+++ Q.magicImage/Q.magicImage.php
@@ -13,9 +13,9 @@
  * @copyright qbbr, 2010
  */
 
-define("BASE_DIR", dirname(dirname(__FILE__)));
+define("BASE_DIR", dirname(dirname(__FILE__))); // root dir /
 define("CACHE_DIR", BASE_DIR.DIRECTORY_SEPARATOR.'etc'.DIRECTORY_SEPARATOR.'cache'.DIRECTORY_SEPARATOR); // if def cache = true; else = false
-define("COPYRIGHT", false);
+define("COPYRIGHT_PIC", BASE_DIR.DIRECTORY_SEPARATOR.'etc'.DIRECTORY_SEPARATOR.'img'.DIRECTORY_SEPARATOR.'copyright.png'); // optional
 define("COPYRIGHT_OPACITY", 100); // [0 - 100]
 
 function resize($fileURI, $maxWidth = null, $maxHeight = null, $m = false, $wp = false) {
@@ -89,7 +89,7 @@ function resize($fileURI, $maxWidth = null, $maxHeight = null, $m = false, $wp =
 		echo file_get_contents($file);
 		return true;
 	}
-	
+
 	$image_p = imagecreatetruecolor($newWidth, $newHeight);
 
 	if ($mime_type == "image/jpeg" ) {
@@ -102,11 +102,10 @@ function resize($fileURI, $maxWidth = null, $maxHeight = null, $m = false, $wp =
 	imagecopyresampled($image_p, $image, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
 
 	/* copyright --- {{{ */
-	if (COPYRIGHT) {
-		$cf = BASE_DIR.DIRECTORY_SEPARATOR.'etc'.DIRECTORY_SEPARATOR.'img'.DIRECTORY_SEPARATOR.'copyright.png';
-		if (is_file($cf)) {
-			$ci = getimagesize($cf);
-			$c = imagecreatefrompng($cf);
+	if (COPYRIGHT_PIC) {
+		if (is_file(COPYRIGHT_PIC)) {
+			$ci = getimagesize(COPYRIGHT_PIC);
+			$c = imagecreatefrompng(COPYRIGHT_PIC);
 			imagecopymerge($image_p, $c, $newWidth-$ci[0], $newHeight-$ci[1], 0, 0, $ci[0], $ci[1], COPYRIGHT_OPACITY);
 		}
 	}

91f7c9a6bec203758247177daa02d7006b60117e
Sokolov Innokenty <r2.kenny@gmail.com>
2010-04-14 19:45:32 +0400

    Q.cssmin (#fix comment space)

--- Q.cssmin/Q.cssmin.php
+++ Q.cssmin/Q.cssmin.php
@@ -3,7 +3,7 @@
  * Q.cssmin - Cascading Style Sheets Minifier
  *
  * add to htaccess
- * RewriteRule ^.*\.css$ cssmin.php [L]
+ * RewriteRule ^.*\.css$ Q.cssmin.php [L]
  *
  * @author Sokolov Innokenty
  * @copyright qbbr, 2010
@@ -11,7 +11,7 @@
 
 error_reporting(0);
 
-define("BASE_DIR", dirname(__FILE__)); // root dir /
+define("BASE_DIR", $_SERVER['DOCUMENT_ROOT']); // root dir /
 define("CACHE_DIR", BASE_DIR.DIRECTORY_SEPARATOR.'cache'.DIRECTORY_SEPARATOR); // if def cache = true; else = false
 define("CACHE_EXPIRE_TIME", 3600 * 24 * 10); // 10 days
 
@@ -33,7 +33,7 @@ if (is_file($cache_file) && filemtime($cache_file) + CACHE_EXPIRE_TIME > time())
 
 $file = file_get_contents($file_path);
 
-$file = preg_replace("/\/\*[\d\D]*?\*\/|\t+/", " ", $file);
+$file = preg_replace("/\/\*[\d\D]*?\*\/|\t+/", "", $file);
 $file = str_replace(array("\n", "\r", "\t"), "", $file);
 $file = preg_replace("/\s\s+/", " ", $file);
 $file = preg_replace("/\s*({|}|;|:|,|\.|~|=|\+|>|<|\?)\s*/", "$1", $file);

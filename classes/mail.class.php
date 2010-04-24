010b5da3308ff3081bc590ddb2f5abc2854cd6cd
Sokolov Innokenty <r2.kenny@gmail.com>
2010-04-24 22:51:19 +0400

    mail.class.php - send mail

---
+++ classes/mail.class.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * отправка почты
+ * @author Sokolov Innokenty (c) 2010
+ */
+
+class mail {
+
+	/**
+	 * кодировка письма
+	 * @var string
+	 */
+	public $charset = "utf8";
+
+	/**
+	 * кодирование контента письма
+	 * @var string
+	 */
+	private $encoding = "8bit";
+
+	/**
+	 * заголовки
+	 * @var array
+	 */
+	private $headers = array();
+
+	/**
+	 * тема письма
+	 * @var string
+	 */
+	private $subject = "";
+
+	/**
+	 * текст
+	 * @var string
+	 */
+	private $message = "";
+
+
+	/**
+	 * установка заголовка
+	 * @param string $key ключ
+	 * @param string $value значение
+	 * @return bool
+	 */
+	private function set_header($key, $value) {
+		if (empty($key) || empty($value)) return false;
+		
+		$this->headers[$key] = $value;
+		
+		return true;
+	}
+
+
+	/**
+	 * от кого
+	 * @param string $mail почта
+	 * @param string $name ФИО
+	 * @return bool
+	 */
+	public function from($mail, $name = "") {
+		return $this->set_header("From", "$name <$mail>");
+	}
+
+
+	/**
+	 * обратный адрес
+	 * @param string $mail почта
+	 * @param string $name ФИО
+	 * @return bool
+	 */
+	public function reply_to($mail, $name = "") {
+		$mail = preg_replace("/.*<(.*)>/", "$1", $mail);
+		return $this->set_header("Reply-To", "$name <$mail>");
+	}
+
+
+	/**
+	 * кому
+	 * @param string $mail почта
+	 * @return bool
+	 */
+	public function to($mail) {
+		return $this->set_header("To", $this->parse_mail($mail));
+	}
+
+
+	/**
+	 * копия
+	 * @param string $mail почта
+	 * @return bool
+	 */
+	public function cc($mail) {
+		return $this->set_header("Cc", $this->parse_mail($mail));
+	}
+
+	
+	/**
+	 * скрытая копия
+	 * @param string $mail почта
+	 * @return bool
+	 */
+	public function bcc($mail) {
+		return $this->set_header("Bcc", $mail = $this->parse_mail($mail));
+	}
+
+
+	/**
+	 * тема сообщения
+	 * @param string $subject тема письма
+	 * @return bool
+	 */
+	public function subject($subject) {
+		$this->subject = $subject;
+		
+		return true;
+	}
+
+
+	/**
+	 * текст сообщения
+	 * @param string $msg сообщение
+	 * @return bool
+	 */
+	public function message($msg) {
+		$this->message = $msg;
+
+		return true;
+	}
+
+
+	/**
+	 * отправка
+	 * @return bool
+	 */
+	public function send() {
+		if (empty($this->headers)) return false;
+
+		$headers = $this->build_headers();
+
+		return @mail($this->headers["To"], $this->subject, $this->message, $headers);
+	}
+
+
+	/**
+	 * правим почту (запятую), если их несколько
+	 * @param string $mail электронный адрес
+	 * @return bool
+	 */
+	private function parse_mail($mail) {
+		if (empty($mail)) return false;
+		
+		$mails = explode(",", $mail);
+		for ($i = 0; $i < count($mails); $i++) {
+			$mails[$i] = trim($mails[$i]);
+		}
+		return implode(", ", $mails);
+	}
+
+
+	/**
+	 * собираем заголовки
+	 * @return string
+	 */
+	private function build_headers() {
+		if (empty($this->headers["Reply-To"])) {
+			$this->reply_to($this->headers["From"]);
+		}
+
+		$this->set_header("Mime-Version", "1.0");
+		
+		$headers = "";
+		foreach ($this->headers as $key => $value) {
+			$value = trim($value);
+			$headers .= "$key: $value\n";
+		}
+		$headers .= "Content-Type: text/plain; charset=".$this->charset."\n";
+		$headers .= "Content-Transfer-Encoding: ".$this->encoding;
+		
+		return $headers;
+	}
+
+}
+?>
